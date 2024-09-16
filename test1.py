b725665272c4aea4159c0bd54d75512b82332648
Charan <charanbodagala05@gmail.com>
2024-09-16 20:05:01 +0530

    first commit

---
+++ test1.py
@@ -0,0 +1,7 @@
+a = 10
+b = 20
+
+#the bug is below where subraction is done inplace of addition
+#modify the '-' to '+'
+sum = a - b
+print(sum)
---
+++ test2.py
@@ -0,0 +1,7 @@
+a = 10
+b = 30
+
+#there is a bug in line6 where subraction is done inplace of addition
+#modify the '+' to '-'
+sub = a + b
+print(sub)
